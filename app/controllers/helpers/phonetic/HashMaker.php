f90c2c2bd55cd17406104d4fd99eee326e0274b2
Hasib Al Muhaimin <himuhasib@gmail.com>
2014-11-07 19:34:20 +0600

    Fix HashMaker hash function

--- app/controllers/helpers/phonetic/HashMaker.php
+++ app/controllers/helpers/phonetic/HashMaker.php
@@ -20,15 +20,15 @@ class HashMaker {
 		// check if given parameter is a string.
 		if(is_string($bangla))
 		{
-			$search = array('ঈ', 'ঊ', 'খ', 'ঘ', 'ছ', 'ঝ', 'ট', 'ঠ', 'ড', 'ঢ', 'ণ', 'য', 'শ', 'ষ', 'ড়', 'ঢ়', 'ং', 'ৎ', 'ঋ');
+			$search  = array('ঈ', 'ঊ', 'খ', 'ঘ', 'ছ', 'ঝ', 'ট', 'ঠ', 'ড', 'ঢ', 'ণ', 'য', 'শ', 'ষ', 'ড়', 'ঢ়', 'ং', 'ৎ', 'ঋ'   );
 
 			$replace = array('ই', 'উ', 'ক', 'গ', 'চ', 'জ', 'ত', 'থ', 'দ', 'ধ', 'ন', 'জ', 'স', 'স', 'র', 'র', 'ঙ', 'ত', 'রি' );
 
 			$bangla = str_replace($search, $replace, $bangla);
 
-			$search = array('ী', 'ৈ', 'ূ', 'ো', 'ৌ', 'ঁ', '্ব', '্', 'ৃ', 'ঃ');
+			$search = array('ী', 'ৈ', 'ূ', 'ো', 'ৌ', 'ঁ', '্', 'ৃ', 'ঃ');
 
-			$replace =array('ি',  'ই', 'ু',  '',   'উ',   '',  '',    '', 'রি',   '');
+			$replace =array('ি',  'ই', 'ু',  '',   'উ',   '',   '', 'রি',  '');
 
 			$bangla = str_replace($search, $replace, $bangla);
 
