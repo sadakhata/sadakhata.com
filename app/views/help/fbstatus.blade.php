c2e6eba7cae104f6c6a14e3aba1fbdbd81220d1f
Hasib Al Muhaimin <himuhasib@gmail.com>
2014-11-09 20:27:24 +0600

    added privacy policy and fix minor view issue

--- app/views/help/fbstatus.blade.php
+++ app/views/help/fbstatus.blade.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta property="og:title" content="হেল্প | কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="<?php echo $_SERVER['SITE_ADDR']; ?>/help/fbstatus" />
-        
-        <meta property="og:description" content="ভার্চুয়াল দুনিয়া রাঙিয়ে দিন বাংলা ভাষায়। আর বিশ্ব কে জানিয়ে দিন বাংলা ভাষা কতটা মায়াবী। মায়াবী এই বাংলা ভাষার ভাষাশহীদদের প্রতি অজস্র ভালোবাসা...Write Bangla from Any Device online. Using Sada khata you can write Bangla from Mobile without downloading any softwere" />
-        <meta property="og:image" content="{{ asset('/assets/images/icon_128x128.png') }}" />
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta property="og:title" content="হেল্প | কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="<?php echo $_SERVER['SITE_ADDR']; ?>/help/fbstatus" />
+		
+		<meta property="og:description" content="ভার্চুয়াল দুনিয়া রাঙিয়ে দিন বাংলা ভাষায়। আর বিশ্ব কে জানিয়ে দিন বাংলা ভাষা কতটা মায়াবী। মায়াবী এই বাংলা ভাষার ভাষাশহীদদের প্রতি অজস্র ভালোবাসা...Write Bangla from Any Device online. Using Sada khata you can write Bangla from Mobile without downloading any softwere" />
+		<meta property="og:image" content="{{ asset('/assets/images/icon_128x128.png') }}" />
 
-        <title>কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট</title>
-        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/assets/images/favicon.ico') }}" />
-        
-        <link rel="stylesheet" type="text/css" href="{{asset('/assets/bootstrap/css/bootstrap.min.css')}}">
-        <style type="text/css">
+		<title>কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট</title>
+		<link rel="shortcut icon" type="image/x-icon" href="{{ asset('/assets/images/favicon.ico') }}" />
+		
+		<link rel="stylesheet" type="text/css" href="{{asset('/assets/bootstrap/css/bootstrap.min.css')}}">
+		<style type="text/css">
 
 		body{
 			box-shadow:10px 10px 5px #999999;
@@ -26,52 +26,63 @@
 		</style>
 
 
-    </head>
-    
-    <body>
-    	<img src="{{ asset('/assets/images/icon_128x128.png') }}" height="100" width="100" style="float:left; opacity:0.5; position:fixed; padding-top:10px; "/>
-    	<div style="padding-left:100px;">
-            
-            <h1>
-                সাদাখাতা | ফোনেটিক বাংলা কনভার্টার
-            </h1>
-            <p style="color:rgb(20,20,20);">
-                ভার্চুয়াল দুনিয়া রাঙিয়ে দিন বাংলা ভাষায়। আর বিশ্ব কে জানিয়ে দিন বাংলা ভাষা কতটা মায়াবী। মায়াবী এই বাংলা ভাষার ভাষা শহীদদের প্রতি অজস্র ভালোবাসা.....
-            </p>
-        </div>
-        
-    	<div style="padding-right:5px;">
-        <hr size="1" color="#999999" />
-        <h4 align="right">কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট</h4>
-        
-        <div style="text-align:right">
-        	<img src="{{asset('/assets/images/facebook_app_icon.png')}}"  />
-        	<img src="{{ asset('/assets/images/banner.png')}}" height="128" width="295" alt="সাদাখাতা" title="সাদাখাতা"/>
-        	
-            <br />
-            
-        </div>
-        </div>
-        <div style="margin:0px 50px;">
-            <p>
-                এখন সাদাখাতা ফোনেটিক বাংলা কনভার্টার (<a href="http://sadakhata.com">http://sadakhata.com</a>) থেকে সরাসরি ফেসবুকে স্ট্যাটাস আপডেট করতে পারবেন কপি পেস্ট ছাড়াই!!! প্রতিটি মোবাইল ভার্সন এর নিচের দিকে একটা বাটন যোগ করা হয়েছে 'ফেসবুকে স্ট্যাটাস হিসেবে দিন'। এই বাটন এ ক্লিক করলে আপনাকে কনফার্ম করতে বলবে যে আপনি কি আপনার লেখাটাকে স্ট্যাটাস হিসাবে দিতে চান?? কনফার্ম করলেই স্ট্যাটাস আপডেট হয়ে যাবে।
-                <br />
-                <br />
-                <br />
-    
-    
-                প্রথমবার স্ট্যাটাস আপডেট করতে গেলে ফেসবুক আপনার কাছে অনুমতি চাইবে যে আপনি কি সাদাখাতা অ্যাপ ব্যাবহার করতে চান(নিচের এই ছবিটির মতন)?? তখন 'GO TO APP' বাটনে ক্লিক করুণ।
-                <br />
-                <img src="{{asset('/assets/images/auth1.PNG')}}" height="370" width="716" alt="Authentication With Facebook" title="Authentication With Facebook, 1st step" style="padding-top:5px;" />
-                <br />
-                <br />
-                <br />
-                এবং তারপর ফেসবুক আবার আপনার কাছে অনুমতি চাইবে যে সাদাখাতা আপনার ওয়াল বা টাইমলাইন এ আপনার হয়ে প্রকাশ করতে পারবে কিনা(নিচের ছবিটির মতন)। এবার Allow/'অনুমতি দিন' বাটনে ক্লিক করলেই আপনার ফেসবুকের সাথে সাদাখাতা যুক্ত হয়ে যাবে। এরপর আর স্ট্যাটাস আপডেট করার সময় ফেসবুক কোন অনুমতি চাইবে না। কোন সমস্যায় পড়লে <a href="http://facebook.com/sadakhata">ফেসুবক পেইজে</a> আমাদের জানান :)
-                
-                <img src="{{asset('/assets/images/auth2.PNG')}}" height="256" width="716" alt="Authentication With Facebook" title="Authentication With Facebook, 1st step" style="padding-top:5px;" />
-                
-                
-            </p>
-       	</div>
-    </body>
+	</head>
+	
+	<body>
+		<img src="{{ asset('/assets/images/icon_128x128.png') }}" height="100" width="100" style="float:left; opacity:0.5; position:fixed; padding-top:10px; "/>
+		<div style="padding-left:100px;">
+			
+			<h1>
+				সাদাখাতা | ফোনেটিক বাংলা কনভার্টার
+			</h1>
+			<p style="color:rgb(20,20,20);">
+				ভার্চুয়াল দুনিয়া রাঙিয়ে দিন বাংলা ভাষায়। আর বিশ্ব কে জানিয়ে দিন বাংলা ভাষা কতটা মায়াবী। মায়াবী এই বাংলা ভাষার ভাষা শহীদদের প্রতি অজস্র ভালোবাসা.....
+			</p>
+		</div>
+		
+		<div style="padding-right:5px;">
+		<hr size="1" color="#999999" />
+		<h4 align="right">কপি পেস্ট ছাড়াই সাদাখাতা(ফোনেটিক বাংলা কনভার্টার) এর মাধ্যমে ফেসবুকে স্ট্যাটাস আপডেট</h4>
+		
+		<div style="text-align:right">
+			<img src="{{asset('/assets/images/facebook_app_icon.png')}}"  />
+			<img src="{{ asset('/assets/images/banner.png')}}" height="128" width="255" alt="সাদাখাতা" title="সাদাখাতা"/>
+			
+			<br />
+			
+		</div>
+		</div>
+		<div style="margin:0px 50px;">
+			<p>
+				এখন সাদাখাতা ফোনেটিক বাংলা কনভার্টার (<a href="http://sadakhata.com">http://sadakhata.com</a>) থেকে সরাসরি ফেসবুকে স্ট্যাটাস আপডেট করতে পারবেন কপি পেস্ট ছাড়াই!!! প্রতিটি মোবাইল ভার্সন এর নিচের দিকে একটা বাটন যোগ করা হয়েছে 'ফেসবুকে স্ট্যাটাস হিসেবে দিন'। এই বাটন এ ক্লিক করলে আপনাকে কনফার্ম করতে বলবে যে আপনি কি আপনার লেখাটাকে স্ট্যাটাস হিসাবে দিতে চান?? কনফার্ম করলেই স্ট্যাটাস আপডেট হয়ে যাবে।
+				<br />
+				<br />
+				<br />
+	
+	
+				প্রথমবার স্ট্যাটাস আপডেট করতে গেলে ফেসবুক আপনার কাছে অনুমতি চাইবে যে আপনি কি সাদাখাতা অ্যাপ ব্যাবহার করতে চান(নিচের এই ছবিটির মতন)?? তখন <strong>Okay</strong> বাটনে ক্লিক করুণ।
+				<br />
+				<img src="{{asset('/assets/images/auth1.PNG')}}" height="442" width="511" alt="Authentication With Facebook" title="Authentication With Facebook, 1st step" style="padding-top:5px;" />
+				<br />
+				<br />
+				<br />
+				এবং তারপর ফেসবুক আবার আপনার কাছে অনুমতি চাইবে যে সাদাখাতা আপনার ওয়াল বা টাইমলাইন এ আপনার হয়ে প্রকাশ করতে পারবে কিনা(নিচের ছবিটির মতন)। এবার <strong>Okay</strong> বাটনে ক্লিক করলেই আপনার ফেসবুকের সাথে সাদাখাতা যুক্ত হয়ে যাবে। এরপর আর স্ট্যাটাস আপডেট করার সময় ফেসবুক কোন অনুমতি চাইবে না। কোন সমস্যায় পড়লে <a href="http://facebook.com/sadakhata">ফেসুবক পেইজে</a> আমাদের জানান :)
+				
+				<img src="{{asset('/assets/images/auth2.PNG')}}" height="450" width="529" alt="Authentication With Facebook" title="Authentication With Facebook, 1st step" style="padding-top:5px;" />
+			</p>
+			<hr>
+			<center><h2>Privacy Policy</h2></center>
+			<p>
+				1. We don't share what you write on sadakhata.
+				<br><br>
+				2. We store what you write on sadakhata in our database, and give you a unique hashed id. So, no one can see what you wrote on sadakhata.
+				<br><br>
+				3. We don't have ads on our site. So, your data is completely safe from third-party.
+				<br><br>
+				4. After sharing status from sadakhata, we delete that status from our database. So you don't have any privacy issue from sadakhata.
+				<br><br>
+				5. We don't share your status with any company under the hood. So you are safe from profile making.
+			</p> 
+		</div>
+	</body>
 </html>
