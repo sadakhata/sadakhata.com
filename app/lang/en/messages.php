f4b90ffb72141a29e73ceb3369b082abc01881d3
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-01-26 18:02:56 +0600

    Localization finished

--- app/lang/en/messages.php
+++ app/lang/en/messages.php
@@ -10,6 +10,6 @@ return array(
 	'shareOnGooglePlus' => 'Share us on Google Plus',
 	'likeThisPage' => 'Like This Page',
 	'clearInputBox' => 'Clear Input Box',
-	'virtualDuniyaRangiyeDin' => 'Virtual Duniya rangiye din',
-	'description' => 'Sadakhata Phonetic Bangla Converter is an app which you can use from any mobile / iPad / computer / MacBook. It is an online-based converter, so to use it you will need a internet connection. Choose your favorite version from the list below. Then write following the keymap and copy-paste the output where needed. You can also update facebook status directly from sadakhata without any copy paste'
+	'virtualDuniyaRangiyeDin' => 'Colour the virtual life with Bengali language and tell the whole world how adorable the Bengali language is! Lots of love to the martyr\'s of our language movement.',
+	'description' => 'Sadakhata Phonetic Bangla Converter is an app which you can use from any mobile / iPad / computer / MacBook. It is an online-based converter, so to use it you will need an internet connection. Choose your favorite version from the list below. Then write following the keymap and copy-paste the output where needed. You can also update facebook status directly from sadakhata without any copy paste'
 );
