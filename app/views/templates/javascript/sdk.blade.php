231e970c3e3710cd2706a6050f2ea7fb90c21e17
Hasib Al Muhaimin <himuhasib@gmail.com>
2015-04-19 23:26:39 +0600

    Google Analytics Code added

--- app/views/templates/javascript/sdk.blade.php
+++ app/views/templates/javascript/sdk.blade.php
@@ -1,3 +1,4 @@
+<!-- Facebook JS SDK -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -6,7 +7,7 @@
   js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=483263038412364&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
-
+<!-- END Facebook JS SDK -->
 
 <!-- Place this render call where appropriate -->
 <script type="text/javascript">
@@ -16,3 +17,17 @@
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
   })();
 </script>
+<!-- End PlusOne code -->
+
+<!-- Google Analytics Code -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-62027562-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<!-- End Google Analytics code -->
